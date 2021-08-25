46bc241311ad67447b4a7ae98adabfe3c96b6329
SazidAhmed <sazidahmed.official@gmail.com>
2021-08-26 00:28:45 +0600

    user profile details added

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Profile extends Model
 {
     use HasFactory;
+
+    protected $guarded=[];
+    
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
