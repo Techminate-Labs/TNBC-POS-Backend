d65d6446b35d07f6c28f49e3da06100860162df6
SazidAhmed <sazidahmed.official@gmail.com>
2021-10-03 00:49:24 +0600

    id and avaialable added to imtes

--- app/Format/ItemFormat.php
+++ app/Format/ItemFormat.php
@@ -3,9 +3,21 @@
 namespace App\Format;
 
 class ItemFormat{
+    public function isAvailable($data){
+        if($data == 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     public function formatItemList($item){
         return[
             'item_id' => $item->id,
+            'category_id' => $item->category_id,
+            'brand_id' => $item->brand_id,
+            'unit_id' => $item->unit_id,
+            'supplier_id' => $item->supplier_id,
             'category' => $item->category->name,
             'brand' => $item->brand->name,
             'unit' => $item->unit->name,
@@ -18,7 +30,7 @@ class ItemFormat{
             'discount' => $item->discount,
             'inventory' => $item->inventory,
             'expire_date' => $item->expire_date,
-            'available' => $item->available,
+            'available' => $this->isAvailable($item->available),
             'image' => $item->image,
             'created_at'=>$item->created_at->diffForHumans(),
             'updated_at'=>$item->updated_at->diffForHumans()
