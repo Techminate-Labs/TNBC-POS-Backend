8608ddbb3e7fd6beb5052f6fb7055f08382fd8c3
SazidAhmed <sazidahmed.official@gmail.com>
2021-10-20 07:04:02 +0600

    inventory maintain functionalites done

--- app/Http/Controllers/Pos/CartController.php
+++ app/Http/Controllers/Pos/CartController.php
@@ -21,28 +21,4 @@ class CartController extends Controller
     {
         return $this->services->cartAddCustomer($request);
     }
-
-    public function testApi(){
-        $url = 'http://54.183.16.194/bank_transactions?recipient=a5dbcded3501291743e0cb4c6a186afa2c87a54f4a876c620dbd68385cba80d0';
-        $fetch = Http::get($url);
-        $data = json_decode($fetch);
-        // $results = $data['results'][1]['memo'];
-        $results = $data->results;
-
-        $transactions = [];
-        $date = [];
-        foreach($results as $result){
-            $amount = $result->amount;
-            $block = $result->block->created_date;
-            array_push($transactions, $amount);
-            array_push($date, $block);
-            
-        }
-        // return $data;
-        $response = [
-            'transactions' => $transactions,
-            'date' => $date
-        ];
-        return response($response, 200);
-    }
 }
